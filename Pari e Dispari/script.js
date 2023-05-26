eba404ac193febad81ff7ff0cac6e0a720e22429
SteCard <stefanocardillo14@gmail.com>
2023-05-26 19:12:20 +0200

    pari-dispari completo

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -1,47 +1,62 @@
 "use strict";
 
 // PROMPT UTENTE
-let user_choice = prompt("Scrivi pari o dispari").toLowerCase();
+let user_choice = prompt('Scrivi "pari" o "dispari"').toLowerCase();
 document.getElementById('user_choice').innerHTML = `Hai scelto: ${user_choice} `;
+
 
 let user_num = parseInt(prompt("Scrivi un numero da 1 a 5"));
 document.getElementById('user_number').innerHTML = `Hai scelto: ${user_num} `;
 
-// VARIABILI PC
-let pc_num = RandomNumb(1,5);
-document.getElementById('pc_number').innerHTML = `Il computer ha scelto: ${pc_num} `;
+
+// CONTROLLO PARI/DISPARI INSERITO CORRETTAMENTE
+while(user_choice !== 'pari' && user_choice !== 'dispari'){
+    user_choice = prompt('Qualcosa è andato storto, scrivi nuovamente "pari" o "dipari"');
+    
+    document.getElementById('user_choice').innerHTML = `Hai scelto: ${user_choice} `;
+}
+
+// CONTROLLO NUMERO INSERITO CORRETTAMENTE
+while(isNaN(user_num) || user_num < 1 || user_num > 5){
+    user_num = prompt('Qualcosa è andato storto, scrivi nuovamente un numero da 1 a 5');
+    
+    document.getElementById('user_number').innerHTML = `Hai scelto: ${user_num} `;
+}
+
 
 // FUNZIONE PER NUMERO RANDOM PC
-function RandomNumb (min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function randomNumb () {
+    return Math.floor(Math.random() * (6-1)) + 1;
+    // return Math.floor(Math.random() * (max - min + 1)) + min;
+    
 }
+
+// VARIABILE NUMERO PC
+let pc_num = randomNumb();
+document.getElementById('pc_number').innerHTML = `Il computer ha scelto: ${pc_num} `;
 
 // SOMMA
 let somma = user_num + pc_num;
 document.getElementById('total').innerHTML = `Il totale è: ${somma} `;
 
-
-
-
 // FUNZIONE SOMMA
-function risultato(user_num,pc_num){
+function pari_dispari(num){
 
-    let result;
-    let somma = user_num + pc_num
-    
-    if (somma % 2 === 0){
-        result = "pari";
-        console.log(somma + " è pari");
+    if (num % 2 === 0){
+        return "PARI";
     } 
 
-    else{
-        result = "dispari";
-        console.log(somma + " è dispari");
-    }
-
-    return result;
+    return "DISPARI";
 }
 
+// CHIAMO LA FUNZIONE E SALVO IL RISULTATO IN UNA VARIABILE
+let esito = pari_dispari(somma);
+document.getElementById('esito').innerHTML = esito;
+
+// CONTROLLO VINCITORE
+if(esito === user_choice){
+    document.getElementById('winner').innerHTML = "Hai VINTO!";
+}
+else{
+    document.getElementById('winner').innerHTML = "Hai PERSO!";
+}
